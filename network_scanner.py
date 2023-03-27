29723efb3ad94fd70f80b609611d19548d94642c
gemstone-source <sophiajulius5@gmail.com>
2023-03-27 14:22:02 +0300

    network scanner

--- network_scanner.py
+++ network_scanner.py
@@ -26,6 +26,5 @@ def print_result(result_list):
     for client in result_list:
         print(client["ip"] +"\t\t"+ client["mac"])
 
-
 scan_result = scan("192.168.43.1/24")
 print_result(scan_result)
