a2a1bf7de5be006c1a292b0dc8fe6c9942fc7adf
Julius Munisi <hashghost@hashghosts-macbook-pro.local>
2026-06-09 18:03:45 +0300

    scrapper

--- scrapper.py
+++ scrapper.py
@@ -1,41 +1,52 @@
 import time
 
+import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
-
-
-
-
-# on = True
-# while on:
-#     def checking_price():
-#         time.sleep(10)
-#         driver = webdriver.Chrome()
-#         driver.get(url)
-#         price = driver.find_element(By.CLASS_NAME,"amount")
-#         print(price.text)
-#         driver.quit()
-#     checking_price()
-
 
 url = "https://github.com/usernam121"
 driver = webdriver.Chrome()
 driver.get(url)
-res = driver.find_element(By.CLASS_NAME, "repo")
-# print(repos.text)
+repo = "https://github.com/usernam121"
 
-
+# time.sleep(2)
+response = driver.find_elements(By.CLASS_NAME,"repo")
+# time.sleep(2)
 
 links = []
-flink = []
+flinks = []
 
-for i in res:
+def going_for_raw(third_page):
+    driver.get(third_page)
+    raw = driver.find_element(By.CLASS_NAME,"prc-Button-ButtonBase-9n-Xk")
+    raw.click()
+    html = driver.page_source
+    html = f"{html}"
+
+    if "lorem" in html:
+        print("Password found in {third_page}")
+
+def loop(next_page):
+    global a
+    driver.get(next_page)
+    response2 = driver.find_elements(By.CLASS_NAME,"react-directory-truncate")
+
+    for a in response2:
+        pass
+    if "py" in a.text:
+        second_link = f"{next_page}/blob/main/{a.text}"
+        # print(second_link)
+        going_for_raw(second_link)
+
+
+for i in response:
     links.append(i.text)
-print(links)
+# print(links)
 
 for l in links:
-    next_page = f"{url}/{l}"
-    flink.append(next_page)
-print(flink)
+    next_page = f"{repo}/{l}"
+    flinks.append(next_page)
+    loop(next_page)
+# print(flinks)
+
 driver.quit()
